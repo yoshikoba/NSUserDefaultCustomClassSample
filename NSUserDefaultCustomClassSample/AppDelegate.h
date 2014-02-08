c1a42a07abb4ed065620a10616c7675b55682f89
yoshikoba <dev.mac.kazukoba@gmail.com>
2014-02-08 09:56:47 +0900

    Initial Commit

---
+++ NSUserDefaultCustomClassSample/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  NSUserDefaultCustomClassSample
+//
+//  Created by kazukoba on 2014/02/08.
+//  Copyright (c) 2014年 WalterAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ NSUserDefaultCustomClassSample/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  NSUserDefaultCustomClassSample
+//
+//  Created by kazukoba on 2014/02/08.
+//  Copyright (c) 2014年 WalterAlex. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
